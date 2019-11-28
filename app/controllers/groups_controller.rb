28172a7bcfd38d1921d15c8c0dcaafef98b136d4
yuchel-246 <yuya.f1117@icloud.com>
2019-11-28 15:34:29 +0900

    グループ編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,8 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+
+      redirect_to group_messages_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
@@ -19,7 +20,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
