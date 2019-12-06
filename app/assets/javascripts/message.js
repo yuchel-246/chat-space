f58370ed337ce4f383f1882142f965ac763454f0
yuchel-246 <yuya.f1117@icloud.com>
2019-12-06 11:47:40 +0900

    コメントアウト削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -24,12 +24,11 @@ $(function(){
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    // console.logを用いてイベント発火しているか確認
     var formdata = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: "POST",  //同期通信でいう『HTTPメソッド』
+      url: url, 
+      type: "POST",  
       data: formdata,  
       dataType: 'json',
       processData: false,
@@ -49,23 +48,17 @@ $(function(){
 
 
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       last_message_id = $('.messages:last').data('message-id');
       
       $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: 'api/messages',
-        //ルーティングで設定した通りhttpメソッドをgetに指定
         type: 'get',
         dataType: 'json',
-        //dataオプションでリクエストに値を含める
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        // console.log(messages)
         var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         messages.forEach(function (message){
           insertHTML = buildHTML(message);
           $('.messages__main').append(insertHTML);
@@ -73,10 +66,9 @@ $(function(){
             scrollTop: $('.messages__main')[0].scrollHeight
           }, 10);
         })
-        //メッセージが入ったHTMLに、入れ物ごと追加
       })
       .fail(function() {
-        console.log('error');
+        alert('自動更新に失敗しました');
       });
     }
   };
@@ -85,8 +77,3 @@ $(function(){
 
 
 
-// if (messages.length !== 0) {
-//   insertHTML += buildHTML(messages)
-//   $('.messages__main').append(insertHTML);
-//   $('.messages__main').animate({ scrollTop: $('.messages__main')[0].scrollHeight});
-// }
