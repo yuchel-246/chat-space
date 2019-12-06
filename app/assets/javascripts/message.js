d3b0b0796731eff3af1b82d72be18e41f932274a
yuchel-246 <yuya.f1117@icloud.com>
2019-12-06 09:59:15 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,40 +1,27 @@
 $(function(){
   function buildHTML(message){
+
+    var message_image = ''
     if ( message.image ) {
+        message_image = `<img src=${message.image}>`
+    }
+
       var html =
-      `<div class="messages">
-        <div class="messages__main">
-          <div class="message-top__sender">
-            ${message.user_name}
-          </div>
-          <div class="message-top__date">
-            ${message.date}
-          </div>
-          <div class="message-top__details">
-            ${message.content}
-          </div>
-          <img src=${message.image} >
-        </div>
-      </div>`
+        ` <div class="messages" data-message-id="${message.id}">
+            <div class="message-top__sender">
+              ${message.user_name}
+            </div>
+            <div class="message-top__date">
+              ${message.date}
+            </div>
+            <div class="message-top__details">
+              ${message.content}
+            </div>
+            ${message_image} 
+          </div>`
       return html;
-    } else {
-      var html =
-      `<div class="messages">
-        <div class="messages__main">
-          <div class="message-top__sender">
-            ${message.user_name}
-          </div>
-          <div class="message-top__date">
-            ${message.date}
-          </div>
-          <div class="message-top__details">
-          ${message.content}
-          </div>
-        </div>
-      </div>`
-      return html;
-    };
-  }
+    } 
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     // console.logを用いてイベント発火しているか確認
@@ -50,7 +37,7 @@ $(function(){
     })
     .done(function(message){
       var html = buildHTML(message);
-      $(".messages").append(html);
+      $(".messages__main").append(html);
       $('form')[0].reset();
       $('.messages__main').animate({ scrollTop: $('.messages__main')[0].scrollHeight});
       $('.send__btn').prop('disabled', false);
@@ -58,5 +45,48 @@ $(function(){
     .fail(function() {
       alert("メッセージ送信に失敗しました");
   });
-  })
+  }) 
+
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      last_message_id = $('.messages:last').data('message-id');
+      
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: 'api/messages',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        // console.log(messages)
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        messages.forEach(function (message){
+          insertHTML = buildHTML(message);
+          $('.messages__main').append(insertHTML);
+          $('.messages__main').animate({
+            scrollTop: $('.messages__main')[0].scrollHeight
+          }, 10);
+        })
+        //メッセージが入ったHTMLに、入れ物ごと追加
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
+
+
+
+// if (messages.length !== 0) {
+//   insertHTML += buildHTML(messages)
+//   $('.messages__main').append(insertHTML);
+//   $('.messages__main').animate({ scrollTop: $('.messages__main')[0].scrollHeight});
+// }
